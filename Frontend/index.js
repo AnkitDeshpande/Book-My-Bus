cfa6364586c1a1ed3b197eceb812007defc91323
Ravi Sharma <sraa7877@gmail.com>
2023-08-23 23:51:06 +0530

    nav bar done

---
+++ Frontend/index.js
@@ -0,0 +1,22 @@
+const divtoshow = 'nav .details .div2 ul'
+const divpopup = document.querySelector(divtoshow);
+const divTrigger = document.querySelector('.account')
+divTrigger.addEventListener('click',()=>{
+  console.log(!divpopup.classList.contains('show'))
+  
+  setTimeout(()=>{
+    if(!divpopup.classList.contains('show')){
+      divpopup.classList.add('show')
+    }
+  },250)
+})
+document.addEventListener('click',(e)=>{
+  const isClosest = e.target.closest(divtoshow)
+  if(!isClosest && divpopup.classList.contains('show')){
+    divpopup.classList.remove('show')
+  }
+})
+
+
+
+
