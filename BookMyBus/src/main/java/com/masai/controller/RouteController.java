a75f81ab343a8c12ddbd3ae767ba14dd84c4b168
Ankit Deshpande <115605876+AnkitDeshpande@users.noreply.github.com>
2023-08-27 00:01:26 +0530

    Update RouteController.java

--- BookMyBus/src/main/java/com/masai/controller/RouteController.java
+++ BookMyBus/src/main/java/com/masai/controller/RouteController.java
@@ -7,25 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-
-
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.masai.exception.ResourceNotFoundException;
@@ -33,6 +19,7 @@ import com.masai.exception.SomethingWentWrongException;
 import com.masai.exception.ValidationException;
 import com.masai.model.Route;
 import com.masai.service.RouteService;
+
 import jakarta.validation.Valid;
 
 @CrossOrigin
