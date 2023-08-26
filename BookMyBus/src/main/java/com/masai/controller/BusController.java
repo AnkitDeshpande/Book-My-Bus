7956a1412319e2823d01cced045acf3dd5bed72c
Ankit Deshpande <urstrulyankit@protonmail.com>
2023-08-26 23:51:36 +0530

    cross origin added

--- BookMyBus/src/main/java/com/masai/controller/BusController.java
+++ BookMyBus/src/main/java/com/masai/controller/BusController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,7 +26,7 @@ import com.masai.model.Bus;
 import com.masai.service.BusService;
 
 import jakarta.validation.Valid;
-
+@CrossOrigin
 @RestController
 public class BusController {
 
--- BookMyBus/src/main/java/com/masai/controller/ReservationController.java
+++ BookMyBus/src/main/java/com/masai/controller/ReservationController.java
@@ -1,19 +1,27 @@
 package com.masai.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.masai.exception.ResourceNotFoundException;
+import com.masai.exception.SomethingWentWrongException;
+import com.masai.exception.ValidationException;
 import com.masai.model.Reservation;
 import com.masai.service.ReservationService;
 
 import jakarta.validation.Valid;
 
-import com.masai.exception.ResourceNotFoundException;
-import com.masai.exception.SomethingWentWrongException;
-import com.masai.exception.ValidationException;
-import java.util.List;
-
+@CrossOrigin
 @RestController
 public class ReservationController {
 	@Autowired
