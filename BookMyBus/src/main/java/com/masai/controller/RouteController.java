ac4ff57ea7e2f510a80c09585f5fd7b816ec2508
Ankit Deshpande <urstrulyankit@protonmail.com>
2023-08-27 20:07:25 +0530

    fetching apis and signups

--- BookMyBus/src/main/java/com/masai/controller/RouteController.java
+++ BookMyBus/src/main/java/com/masai/controller/RouteController.java
@@ -11,7 +11,9 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.masai.exception.ResourceNotFoundException;
@@ -32,8 +34,9 @@ public class RouteController {
 	@GetMapping("/api/routes")
 	public ResponseEntity<List<Route>> getAllRoutes(@RequestParam(required=false) Integer page,@RequestParam(required=false) Integer limit,@RequestParam(required=false) String order,@RequestParam(required=false) String sort) {
 		List<Route> routes = routeService.getAllRoutes();
+		List<Route> paginationAndSort = null;
 		if(page!=null&&limit!=null) {
-			List<Route> paginationAndSort = routeService.paginationAndSort(page, limit);
+			paginationAndSort = routeService.paginationAndSort(page, limit);
 			return new ResponseEntity<>(paginationAndSort, HttpStatus.OK);
 		}
 		else if(!sort.isBlank()&&!order.isBlank()) {
@@ -64,7 +67,6 @@ public class RouteController {
 		routeService.deleteRoute(id);
 		return new ResponseEntity<>("Deleted", HttpStatus.NO_CONTENT);
 	}
-<<<<<<< HEAD
 	
 	@PutMapping("/api/routes/{id}")
 	public ResponseEntity<Route> updateRoute(@Valid @RequestBody Route route)
@@ -72,8 +74,4 @@ public class RouteController {
 		Route updateRoute = routeService.updateRoute(route);
 		return new ResponseEntity(updateRoute, HttpStatus.ACCEPTED);
 	}
-		
-
-=======
->>>>>>> 7df823493c73fbbf3dd3c4933a6408f7a0b5ad8d
 }
--- Frontend/login-signup.js
+++ Frontend/login-signup.js
@@ -1,9 +1,7 @@
 // const baseServerURL = `http://localhost:8888`;
 // const userRegisterURL = `${baseServerURL}/customers`;
 
-
-
-const signinbtn = document.querySelector('.signinbtn')
+const signinbtn = document.querySelector(".signinbtn");
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
@@ -15,119 +13,111 @@ const sign_up_admin = document.querySelector("#sign-up-admin");
 const sign_up_user = document.querySelector("#sign-up-user");
 signinbtn.addEventListener("click", loginUser);
 
-
 sign_up_btn.addEventListener("click", () => {
     container.classList.add("sign-up-mode");
 });
 sign_in_btn.addEventListener("click", () => {
     container.classList.remove("sign-up-mode");
 });
-sign_up_btn2.addEventListener("click", () => {
-    container.classList.add("sign-up-mode2");
-});
-sign_in_btn2.addEventListener("click", () => {
-    container.classList.remove("sign-up-mode2");
-});
-sign_up_admin.addEventListener("click", () => {
-    container1.classList.add("display");
-    container2.classList.remove("display");
-});
-sign_up_user.addEventListener("click", () => {
-    container1.classList.remove("display");
-    container2.classList.add("display");
-});
+// sign_up_btn2.addEventListener("click", () => {
+//     container.classList.add("sign-up-mode2");
+// });
+// sign_in_btn2.addEventListener("click", () => {
+//     container.classList.remove("sign-up-mode2");
+// });
+// sign_up_admin.addEventListener("click", () => {
+//     container1.classList.add("display");
+//     container2.classList.remove("display");
+// });
+// sign_up_user.addEventListener("click", () => {
+//     container1.classList.remove("display");
+//     container2.classList.add("display");
+// });
 
+const firstname = document.querySelector(".firstname");
+const lastname = document.querySelector(".lastname");
+const mobileno = document.querySelector(".mobileno");
+const email = document.querySelector(".email");
+const password = document.querySelector(".password");
+const signupbtn = document.querySelector(".signupbtn");
 
+signupbtn.addEventListener("click", async (e) => {
+    e.preventDefault();
 
-
-const firstname = document.querySelector(".firstname")
-const lastname = document.querySelector(".lastname")
-const mobileno = document.querySelector(".mobileno")
-const email = document.querySelector(".email")
-const password = document.querySelector(".password")
-const signupbtn = document.querySelector('.signupbtn')
-
-
-signupbtn.addEventListener('click', (e) => {
-    e.preventDefault()
-    let userObj={
-        firstName: firstname.value,
-        lastName: lastname.value,
+    const userObj = {
+        // userLoginId: 2,
+        userName: firstname.value,
+        name: lastname.value,
         email: email.value,
         password: password.value,
-        mobileNumber: mobileno.value,
-      }
-      console.log(userObj)
-      fetch('http://localhost:8888/customers',{
-        method:"POST",
-        headers:{
-          "Content-type":"application/json"
+        contact: mobileno.value,
+        role: "user",
+    };
+
+    fetch("http://localhost:8080/api/users", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
         },
-        body:JSON.stringify(userObj)
-      })
-      .then(res => res.json())
-      .then(data => console.log(data))
-      .catch(error =>{
-      console.log(error)
-      })
-})
+        body: JSON.stringify(userObj),
+    })
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+        .catch((error) => {
+            console.log(error);
+        });
+});
 
+function loginUser(e) {
+    e.preventDefault();
 
-  
-function loginUser(e){
+    var email = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
 
-      e.preventDefault()
+    console.log(email, password);
+    // Make GET request to fetch JWT token
+    var authHeader = "Basic " + btoa(email + ":" + password);
 
-    
-      var email = document.getElementById("username").value
-      var password = document.getElementById("password").value
-      
-    console.log(email,password)
-      // Make GET request to fetch JWT token
-      var authHeader = "Basic " + btoa(email + ":" + password);
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Authorization", authHeader);
 
-      var myHeaders = new Headers();
-      myHeaders.append("Content-Type", "application/json");
-      myHeaders.append("Authorization", authHeader);
-    
-      // Set up the fetch options with the Basic Authentication header
-      var requestOptions = {
-          method: "GET",
-          headers: myHeaders,
-          redirect: "follow",
-      };
-    
-      const varification = "";
-      // Send the GET request to the /signIn endpoint
-      fetch('http://localhost:8888/signIn', requestOptions)
-      .then((response) => {
-        if (response.ok) {
-          var token = response.headers.get("Authorization");
-          localStorage.setItem("jwtToken", JSON.stringify(token));
-          console.log("Token stored:", token);
-        } else {
-          console.log("Error:", response.status);
-        }
-        return response.json();
-      })
-      .then((result) => {
-        var customerId = result.customerId;
-        var customername = result.name;
-        var cart= result.cart;
-        localStorage.setItem("cart", JSON.stringify(cart));
-        localStorage.setItem("customername", customername);
-        console.log(result);
-        console.log(result.role);
-        if(result.role == 'ROLE_USER'){
-          alert("user login successfull")
-          window.location.href = "index.html";
-        }else{
-          alert("admin login successfull")
-          window.location.href = "admin.html";
-        }
-        localStorage.setItem("customerId", customerId);
-      })
-      .catch((error) => console.log("error", error));
-  
-}  
-  
+    // Set up the fetch options with the Basic Authentication header
+    var requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow",
+    };
+
+    const varification = "";
+    // Send the GET request to the /signIn endpoint
+    fetch("http://localhost:8888/signIn", requestOptions)
+        .then((response) => {
+            if (response.ok) {
+                var token = response.headers.get("Authorization");
+                localStorage.setItem("jwtToken", JSON.stringify(token));
+                console.log("Token stored:", token);
+            } else {
+                console.log("Error:", response.status);
+            }
+            return response.json();
+        })
+        .then((result) => {
+            var customerId = result.customerId;
+            var customername = result.name;
+            var cart = result.cart;
+            localStorage.setItem("cart", JSON.stringify(cart));
+            localStorage.setItem("customername", customername);
+            console.log(result);
+            console.log(result.role);
+            if (result.role == "ROLE_USER") {
+                alert("user login successfull");
+                window.location.href = "index.html";
+            } else {
+                alert("admin login successfull");
+                window.location.href = "admin.html";
+            }
+            localStorage.setItem("customerId", customerId);
+        })
+        .catch((error) => console.log("error", error));
+}
