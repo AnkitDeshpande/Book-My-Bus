9e0005b62ce0321c57ee707de31e88e9a9ab5164
Mahendra Chandravanshi <101566258+mahendravanshi@users.noreply.github.com>
2023-08-28 10:45:18 +0530

    Update admin.js

--- Frontend/admin.js
+++ Frontend/admin.js
@@ -10,14 +10,57 @@ const busesList = document.getElementById("buses-list");
 const addButton = document.getElementById("add-button");
 const busNameInput = document.getElementById("bus-name");
 
-let arr = [];
+const addBusForm = document.getElementById("add-bus-form");
+
+
+addBusForm.addEventListener("submit", async event => {
+    event.preventDefault();
+
+    const busNameInput = document.getElementById("busNameInput");
+const busTypeInput = document.getElementById("busTypeInput");
+const routeFromInput = document.getElementById("routeFromInput");
+const routeToInput = document.getElementById("routeToInput");
+const arrivalTimeInput = document.getElementById("arrivalTimeInput");
+const departureTimeInput = document.getElementById("departureTimeInput");
+const seatsInput = document.getElementById("seatsInput");
+const availableSeatsInput = document.getElementById("availableSeatsInput");
+const driverNameInput = document.getElementById("driverNameInput");
+const imageInput = document.getElementById("imageInput");
+
+    const newBusData = {
+        busName: busNameInput.value.trim(),
+        busType: busTypeInput.value.trim(),
+        routeFrom: routeFromInput.value.trim(),
+        routeTo: routeToInput.value.trim(),
+        arrivalTime: arrivalTimeInput.value,
+        departureTime: departureTimeInput.value,
+        seats: parseInt(seatsInput.value),
+        availableSeats: parseInt(availableSeatsInput.value),
+        driverName: driverNameInput.value.trim(),
+        image: imageInput.value.trim(),
+        deleted: false, // Set to false by default
+    };
+
+    if (newBusData.busName) {
+        try {
+            await createBus(newBusData);
+            addBusForm.reset();
+            renderBuses();
+        } catch (error) {
+            console.error("Error creating bus:", error);
+        }
+    }
+});
+
+
+
+
+
 
 async function fetchBuses() {
     try {
-        console.log("inside fetch buses");
         const response = await fetch(baseUrl);
         const buses = await response.json();
-        console.log(buses);
         return buses;
     } catch (error) {
         console.error("Error fetching buses:", error);
@@ -25,80 +68,85 @@ async function fetchBuses() {
     }
 }
 
+async function updateBus(id, updatedData) {
+    const response = await fetch(`${baseUrl}/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedData),
+    });
+    const updatedBus = await response.json();
+    return updatedBus;
+}
 
-fetchBuses();
 
-// async function createBus(busData) {
-//     const response = await fetch(baseUrl, {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify(busData),
-//     });
-//     const createdBus = await response.json();
-//     return createdBus;
-// }
+async function addBus(){
 
-// async function updateBus(id, updatedData) {
-//     const response = await fetch(`${baseUrl}/${id}`, {
-//         method: "PUT",
-//         headers: {
-//             "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify(updatedData),
-//     });
-//     const updatedBus = await response.json();
-//     return updatedBus;
-// }
+     
+}
 
-// async function deleteBus(id) {
-//     const response = await fetch(`${baseUrl}/${id}`, {
-//         method: "DELETE",
-//     });
-//     return response.status === 204;
-// }
+async function renderBuses() {
+    const buses = await fetchBuses();
+    busesList.innerHTML = "";
 
-// async function renderBuses() {
-//     const buses = await fetchBuses();
-//     busesList.innerHTML = "";
-    
-//     buses.forEach(bus => {
-//         const li = document.createElement("li");
-//         li.textContent = bus.busName;
+    buses.forEach(bus => {
+        const busElement = document.createElement("li");
+        busElement.classList.add("busElement");
+        busElement.innerHTML = `
+            
+            <div>${bus.busName}</div>
+            // <img src="${bus.image}" alt="${bus.busName} Image">
         
-//         const updateButton = document.createElement("button");
-//         updateButton.textContent = "Update";
-//         updateButton.addEventListener("click", async () => {
-//             const updatedName = prompt("Enter the updated name for the bus:");
-//             if (updatedName) {
-//                 await updateBus(bus.busId, { busName: updatedName });
-//                 renderBuses();
-//             }
-//         });
-        
-//         const deleteButton = document.createElement("button");
-//         deleteButton.textContent = "Delete";
-//         deleteButton.addEventListener("click", async () => {
-//             const deleted = await deleteBus(bus.busId);
-//             if (deleted) {
-//                 renderBuses();
-//             }
-//         });
-        
-//         li.appendChild(updateButton);
-//         li.appendChild(deleteButton);
-//         busesList.appendChild(li);
-//     });
-// }
+                <div><strong>Bus Type:</strong> ${bus.busType}</div>
+                <div><strong>Route From:</strong> ${bus.routeFrom}</div>
+                <div><strong>Route To:</strong> ${bus.routeTo}</div>
+                <div><strong>Arrival Time:</strong> ${bus.arrivalTime}</div>
+                <div><strong>Departure Time:</strong> ${bus.departureTime}</div>
+                <div><strong>Seats:</strong> ${bus.seats}</div>
+                <div><strong>Available Seats:</strong> ${bus.availableSeats}</div>
+                <div><strong>Driver Name:</strong> ${bus.driverName}</div>
+                <div><strong>Deleted:</strong> ${bus.deleted}</div>
+            
+            <button class="update-button" data-id="${bus.busId}">Update</button>
+            <button class="delete-button" data-id="${bus.busId}">Delete</button>
+        `;
 
-// addButton.addEventListener("click", async () => {
-//     const name = busNameInput.value.trim();
-//     if (name) {
-//         await createBus({ busName: name });
-//         busNameInput.value = "";
-//         renderBuses();
-//     }
-// });
+        const updateButton = busElement.querySelector(".update-button");
+        updateButton.addEventListener("click", async () => {
+            const updatedData = {
+                arrivalTime: prompt("Enter Arrival Time:", bus.arrivalTime),
+                availableSeats: parseInt(prompt("Enter Available Seats:", bus.availableSeats)),
+                busName: prompt("Enter Bus Name:", bus.busName),
+                busType: prompt("Enter Bus Type:", bus.busType),
+                deleted: prompt("Enter bus status: ",bus.deleted),
+                departureTime: prompt("Enter Departure Time:", bus.departureTime),
+                driverName: prompt("Enter Driver Name:", bus.driverName),
+                image: prompt("Enter Image URL:", bus.image),
+                routeFrom: prompt("Enter Route From:", bus.routeFrom),
+                routeTo: prompt("Enter Route To:", bus.routeTo),
+                seats: parseInt(prompt("Enter Seats:", bus.seats))
+            };
+            
+            const updatedBus = await updateBus(bus.busId, updatedData);
+            console.log("Updated Bus:", updatedBus);
+            renderBuses();
+        });
 
-// renderBuses();
+
+        const deleteButton = busElement.querySelector(".delete-button");
+
+        deleteButton.addEventListener("click",async()=>{
+             
+            const updatedData = {deleted:true};
+            const updatedBus = await updateBus(bus.busId, updatedData);
+            console.log("Updated Bus:", updatedBus);
+            renderBuses();
+
+        });
+
+        busesList.appendChild(busElement);
+    });
+}
+
+renderBuses();
